ef62d8992bf16ab8c71734022917eaee5cd29f82
RezaBG <101470327+RezaBG@users.noreply.github.com>
2025-04-29 11:13:44 +0200

    feat: add season management model with enums for season name, type, country, gender, and round

---
+++ models/season.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Season struct {
+	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+
+	Name       SeasonNameEnum `gorm:"type:varchar(100);not null"`
+	Country    CountryEnum    `gorm:"type:varchar(50);not null"`
+	Gender     GenderEnum     `gorm:"type:varchar(10);not null"`
+	SeasonType SeasonTypeEnum `gorm:"type:varchar(20);not null"`
+	SeasonYear string         `gorm:"type:varchar(10);not null"` // Example: "2024-2025"
+
+	StartDate *time.Time `gorm:"type:timestamp"`
+	EndDate   *time.Time `gorm:"type:timestamp"`
+
+	Round RoundEnum `gorm:"type:varchar(30);not null"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
